ec8ca3438367e6003ae127b90b32ab8a0aaeb5d9
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2024-05-20 23:31:27 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.wasm$/,
+            type: "javascript/auto",
+            use: [
+                {
+                    loader: "file-loader",
+                    options: {
+                        publicPath: "/_next/static/wasm/",
+                        outputPath: "static/wasm/",
+                        name: "[name].[ext]",
+                    },
+                },
+            ],
+        });
+
+        return config;
+    },
+    async headers() {
+        return [
+            {
+                source: "/_next/static/wasm/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
